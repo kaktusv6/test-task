502f51538d2d08b1649c2d3045af6824c152ca00
kaktusv6 <kaktusv6@gmail.com>
2019-11-03 16:11:04 +1000

    Изменил содержимое ошибки NotValidateValue

--- src/Exceptions/NotValidateValue.php
+++ src/Exceptions/NotValidateValue.php
@@ -3,15 +3,13 @@
 
 namespace Exceptions;
 
-
 use Exception;
 use Throwable;
 
 class NotValidateValue extends Exception {
 
-  public function __construct($type, $code = 0, Throwable $previous = null)
-  {
-    $message = "Not validate value type of $type";
+  public function __construct($value, $type, $code = 0, Throwable $previous = null) {
+    $message = "Not validate value '$value' type of '$type'";
     parent::__construct($message, $code, $previous);
   }
 }
