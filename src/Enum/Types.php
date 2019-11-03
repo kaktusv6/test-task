bd8759d73118accb09833885dea95c0acb9e1991
kaktusv6 <kaktusv6@gmail.com>
2019-11-03 16:09:06 +1000

    Реализовал Enum для типов

---
+++ src/Enum/Types.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Enum;
+
+class Types {
+  const INT = "целое число";
+  const FLOAT = "число с плавающей точкой";
+  const STRING = "строка";
+  const PHONE = "номер телефона";
+}
