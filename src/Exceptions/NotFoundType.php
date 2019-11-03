51f61ce32c6d25ba945f7996cbec1b6a58df991f
kaktusv6 <kaktusv6@gmail.com>
2019-11-03 16:10:20 +1000

    Изменил содержимое ошибки NotFoundType

--- src/Exceptions/NotFoundType.php
+++ src/Exceptions/NotFoundType.php
@@ -3,7 +3,12 @@
 namespace Exceptions;
 
 use Exception;
+use Throwable;
 
 class NotFoundType extends Exception {
-  protected $message = 'Not found type';
+
+  public function __construct($type, $code = 0, Throwable $previous = null) {
+    $message = "Not found type $type";
+    parent::__construct($message, $code, $previous);
+  }
 }
