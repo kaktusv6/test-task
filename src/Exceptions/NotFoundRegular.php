90194d78e0d0a7bd920ab6c3a116a0df8d6dd185
kaktusv6 <kaktusv6@gmail.com>
2019-11-03 16:09:56 +1000

    Изменил сообщение ошибки

--- src/Exceptions/NotFoundRegular.php
+++ src/Exceptions/NotFoundRegular.php
@@ -3,9 +3,12 @@
 
 namespace Exceptions;
 
-
 use Exception;
+use Throwable;
 
 class NotFoundRegular extends Exception {
-  protected $message = 'Not found regular';
+  public function __construct($regular, $code = 0, Throwable $previous = null) {
+    $message = "Not found regular $regular";
+    parent::__construct($message, $code, $previous);
+  }
 }
