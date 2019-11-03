3a9690aed0503dda8f9607b98b6fbfddb29da7bc
kaktusv6 <kaktusv6@gmail.com>
2019-11-03 16:14:41 +1000

    Изменил скрипт примера работы санитайзера

--- src/index.php
+++ src/index.php
@@ -7,8 +7,8 @@ use \Services\SanitizerService;
 $service = new SanitizerService();
 
 $testPost = [
-  'data_json' => '{"foo": "123", "bar": "asd", "baz": "8 (950) 288-56-23"}',
-  'types_json' => '{"foo": "целое число", "bar": "строка", "baz": "номер телефона"}'
+  'data_json' => '{"foo": "123.234", "bar": "asd", "baz": "8 (950) 288-56-23"}',
+  'types_json' => '{"foo": "число с плавающей точкой", "bar": "строка", "baz": "номер телефона"}'
 ];
 $dataRequest = $service->getDataFromRequest($testPost);
 $dataObject = $service->generateObject($dataRequest['data'], $dataRequest['types']);
